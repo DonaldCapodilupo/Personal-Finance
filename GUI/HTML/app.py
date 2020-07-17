5cfb6d7dbf828a0c7a65d36d2d44303ea6bbb936
DonaldCapodilupo <Donald@Capodilupo.dev>
2020-07-17 01:21:41 -0400

    App now sucessfully returns the value in the entry field of the "AddAccount".html"

--- GUI/HTML/app.py
+++ GUI/HTML/app.py
@@ -1,12 +1,24 @@
 import eel
+from Classes.DatabaseRetrieval import DatabaseManipulation
 
 
 eel.init('web')
-
+eel.start('main.html', block=False)
 
 
 @eel.expose
-def my_python_method(parm1,parm2):
-    print(parm1+parm2)
+def send(msg):
+    print("Received Message: " + msg)
+    return "ok"
 
-eel.start('main.html')
+while True:
+    text = eel.readTextBox()()
+    value = eel.readValueBox()()
+    #accountType = eel.readRadioButtons()()
+    print("Text box contents: {}".format(text))
+    eel.sleep(2.0)
+    print("Value box contents: {}".format(value))
+    eel.sleep(2.0)
+
+
+
